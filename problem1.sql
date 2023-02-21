2f2d99371fef58a24833a24669430feb12f67577
Yifan Qiu <yif.chu22@gmail.com>
2023-02-21 19:44:48 +0000

    insert

---
+++ problem1.sql
@@ -0,0 +1,17 @@
+INSERT INTO song (
+    title,
+    artist
+) 
+VALUES
+    (
+        'Let it be', 
+        'Beatle'
+    ),
+    (
+        'Hotel California', 
+        'Eagles'
+    ),
+    (
+        'Kashmir',
+        'Led Zeppelin'
+    );
